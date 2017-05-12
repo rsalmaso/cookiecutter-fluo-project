c38409c5d32e0c5a0b342c2579f9d50b1048bb08
Raffaele Salmaso <raffaele@salmaso.org>
2017-05-12 18:32:26 +0200

    load admin views

--- {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/apps.py
+++ {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/apps.py
@@ -1,10 +1,15 @@
 from django.apps import AppConfig
+from django.utils.module_loading import import_module
 from django.utils.translation import gettext_lazy as _
 from fluo.apps import FluoAdminConfig
 
 
 class {{ cookiecutter.camel_case_app_name }}AdminConfig(FluoAdminConfig):
     default_site = "{{ cookiecutter.project_name }}.admin.sites.AdminSite"
+
+    def ready(self):
+        super().ready()
+        import_module("{{ cookiecutter.project_name }}.admin.views")
 
 
 class {{ cookiecutter.camel_case_app_name }}Config(AppConfig):
