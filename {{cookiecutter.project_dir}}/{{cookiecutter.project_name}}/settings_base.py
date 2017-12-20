6200d75b2045ba7005a686f561c91b9244fa75e8
Raffaele Salmaso <raffaele@salmaso.org>
2017-12-20 22:56:22 +0100

    removed django.contrib.auth.middleware.SessionAuthenticationMiddleware from MIDDLEWARE_CLASSES

--- {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/settings_base.py
+++ {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/settings_base.py
@@ -176,7 +176,6 @@ MIDDLEWARE_CLASSES = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     #"django.middleware.http.ConditionalGetMiddleware",
