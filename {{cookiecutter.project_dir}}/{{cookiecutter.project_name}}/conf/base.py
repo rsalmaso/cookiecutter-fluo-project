ffc041b299a915aac777476e7e9524b41d502004
Raffaele Salmaso <raffaele@salmaso.org>
2019-01-18 18:52:55 +0100

    Always use I18N, L10N and timezone aware datetimes

--- {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/conf/base.py
+++ {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/conf/base.py
@@ -71,18 +71,6 @@ LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", "en")
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-#LOCALE_PATHS = []
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = rel("media")
