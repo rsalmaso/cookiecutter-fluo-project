5ede536e4429e5eba7e15fccef6ea1bd334b3897
Raffaele Salmaso <raffaele@salmaso.org>
2019-01-18 21:01:31 +0100

    Removed comments

--- {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/conf/base.py
+++ {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/conf/base.py
@@ -57,46 +57,21 @@ ADMINS = [
 
 MANAGERS = ADMINS
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
 
 DATABASES = {
     "default": os.environ.get("DATABASES_DEFAULT", os.environ.get("DATABASE_URL", f"postgres://{getpass.getuser()}:@:5432/{{ cookiecutter.db_name }}")),
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = os.environ.get("TIME_ZONE", "Europe/Rome")
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", "en")
 
 SITE_ID = 1
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.environ.get("MEDIA_ROOT", rel("media"))
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = "/media/"
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.environ.get("STATIC_ROOT", rel("static"))
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = "/static/"
 
 # Additional locations of static files
@@ -109,8 +84,6 @@ STATICFILES_DIRS = [
     # lib_rel('fluo', 'static'),
 ]
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
@@ -179,7 +152,6 @@ ROOT_URLCONF = os.environ.get("ROOT_URLCONF", "{{ cookiecutter.project_name }}.u
 # LOGIN_REDIRECT_URL = "/" #"/accounts/profile/"
 AUTH_USER_MODEL = "auth.User"
 
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = os.environ.get("WSGI_APPLICATION", "{{ cookiecutter.project_name }}.wsgi.application")
 
 INSTALLED_APPS = [
@@ -226,11 +198,6 @@ INSTALLED_APPS = [
     "legal.apps.LegalConfig",{% endif %}
 ]
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": True,
