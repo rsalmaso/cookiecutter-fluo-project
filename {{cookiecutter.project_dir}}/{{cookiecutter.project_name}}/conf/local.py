363203b32d1dc93d2d9fffa48365a0daadca9715
Raffaele Salmaso <raffaele@salmaso.org>
2020-02-15 21:34:12 +0100

    Set DEBUG=True in local.py

--- {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/conf/local.py
+++ {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/conf/local.py
@@ -1,5 +1,5 @@
 from .base import *
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
-DEBUG = os.environ.get("DEBUG", "false").lower() in ("true", "1")
+DEBUG = os.environ.get("DEBUG", "true").lower() in ("true", "1")
 SECRET_KEY = os.environ.get("SECRET_KEY", "<%SECRET_KEY%>")
