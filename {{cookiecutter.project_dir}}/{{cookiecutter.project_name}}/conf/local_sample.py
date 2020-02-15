ea418abbd88e019bb40874e12dba7371b99b2e74
Raffaele Salmaso <raffaele@salmaso.org>
2020-02-15 21:35:13 +0100

    Update local_sample.py config

--- {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/conf/local_sample.py
+++ {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/conf/local_sample.py
@@ -1,5 +1,7 @@
+import os
+
 from .base import *
 
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
-# DEBUG = os.environ.get("DEBUG", "false").lower() in ("true", "1")
-# SECRET_KEY = os.environ.get("SECRET_KEY", "<%SECRET_KEY%>")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+DEBUG = os.environ.get("DEBUG", "true").lower() in ("true", "1")
+SECRET_KEY = os.environ.get("SECRET_KEY", None)
