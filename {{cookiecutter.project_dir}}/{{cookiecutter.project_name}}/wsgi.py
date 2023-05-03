1c7941655c8bb8682f6788067dfd255358335581
Raffaele Salmaso <raffaele@salmaso.org>
2023-05-03 22:55:13 +0200

    Cleanup wsgi.py

--- {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/wsgi.py
+++ {{cookiecutter.project_dir}}/{{cookiecutter.project_name}}/wsgi.py
@@ -1,40 +1,9 @@
-"""
-WSGI config for www project.
-
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
 
 import os
-import sys
 
 import service_urls.patch  # noqa: F401 isort:skip
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_name }}.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-from django.core.wsgi import get_wsgi_application  # isort:skip
-
-_application = get_wsgi_application()
-
-
-def application(environ, start_response):
-    if environ["wsgi.url_scheme"] == "https":
-        environ["HTTPS"] = "on"
-    return _application(environ, start_response)
-
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+application = get_wsgi_application()
